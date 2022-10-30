f6103258d199653d06142ce89b947573af26851d
NickDuyH2K3 <96042373+NickDuyH2K3@users.noreply.github.com>
2022-10-30 21:15:41 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -20,6 +20,14 @@ app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/search',(req,res)=>{
+  
+  res.render('search');})
+
+  app.post('/search',(req,res)=>{
+  
+    res.send('');})
+
 app.get('/news', (req, res) => {
   res.render('news');
 })
