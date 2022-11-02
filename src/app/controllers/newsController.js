cae42f39f6cfa38ea0a781d8a34d9ed83f4d7a3a
NickDuyH2K3 <96042373+NickDuyH2K3@users.noreply.github.com>
2022-11-02 22:32:19 +0700

    Redirect Routes and Add Controllers

---
+++ src/app/controllers/newsController.js
@@ -0,0 +1,13 @@
+class newsController
+{
+    index(req, res)
+    {
+        res.render(('news'));
+    }
+    show(req, res)
+    {
+        res.send('News Detail');
+    }
+}
+
+module.exports = new newsController();
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const port = 3000
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 
+const route = require('./routes')
+
+
+
 app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,13 +20,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//route INIT
+route(app)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
