c75600706d908043780f113a44be250af1460782
NickDuyH2K3 <96042373+NickDuyH2K3@users.noreply.github.com>
2022-11-26 13:28:34 +0700

    Adding Catalog Model

---
+++ src/app/models/catalog.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Catalog = new Schema({
+    catalog_id: { type: String, maxLength: 255 },
+    catalog_name: { type: String, maxLength: 255 },
+    catalog_benefit: { type: String, maxLength: 600 },
+    catalog_harvest: { type: String, maxLength: 600 },
+    catalog_original: { type: String, maxLength: 600 },
+    catalog_timeline: { type: String, maxLength: 600 },
+});
+
+module.exports = mongoose.model('Catalog', Catalog);
