27c325805dfccea8376bf1ae6d8c7b2bb1aebfc6
NickDuyH2K3 <96042373+NickDuyH2K3@users.noreply.github.com>
2022-11-27 16:11:29 +0700

    Adding Workshop Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,9 @@
-const Course = require('../models/course');
-const { multipleMongooseToObject } = require('../../utils/mongoose');
 
 class siteController {
     index(req, res, next) {
         res.render('home', {
         style: "home",
         title: "Home | CLOVER ®",
-        });
-    }
-    search(req, res) {
-        res.render('search',{
-            style: "search",
         });
     }
 }
--- src/app/controllers/workshopController.js
+++ src/app/controllers/workshopController.js
@@ -1,9 +1,23 @@
+const Workshop = require('../models/workshop');
+
 class workshopController{
-    index(req, res) {
-        res.render(('layouts/workshop/workshop-index'),{ 
-            style: "workshop",
-            title: "Workshop | CLOVER ®",
+    index(req, res, next) {
+    Workshop.find({})
+        .lean()
+        .then(workshops => {
+            res.render('layouts/workshop/workshop-index', {
+                workshops: workshops,
+                style: "workshop",
+                title: "Workshop | CLOVER ®",
+            })
         });
     }
 }
+
+    // index(req, res) {
+    //     res.render(('layouts/workshop/workshop-index'),{ 
+    //         style: "workshop",
+    //         title: "Workshop | CLOVER ®",
+    //     });
+    // }
 module.exports = new workshopController();
