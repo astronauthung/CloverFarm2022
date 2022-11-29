b91cb50f5d3f9c48cb7c83dec51712b26cd69c32
NickDuyH2K3 <96042373+NickDuyH2K3@users.noreply.github.com>
2022-11-29 21:40:09 +0700

    Changing Admin Panel

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-
-const AdminJS = require('adminjs')
+const express = require('express');
 const mongoose = require('mongoose');
-const AdminJSExpress = require('@adminjs/express')
-const express = require('express')
+const AdminJS = require('adminjs');
+const AdminJSExpress = require('@adminjs/express');
+const AdminJSMongoose = require('@adminjs/mongoose');
+const connectDb = require('../blog/src/config/database');
 
 
-const PORT = 9999
+//models
+const workshop = require('../blog/src/app/models/workshop');
+const catalog = require('../blog/src/app/models/catalog');
 
-const start = async () => {
-  const app = express()
-
-  await mongoose.connect('mongodb+srv://PhucHung:phuchungoccho@cloverfarm2022.4ihfbuf.mongodb.net/test')
-
-  const admin = new AdminJS({
-  })
-
-  const adminRouter = AdminJSExpress.buildRouter(admin)
-  app.use(admin.options.rootPath, adminRouter)
-
-  app.listen(PORT, () => {
-    console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
-  })
+const catalogOption = {
+    resource: catalog,
 }
 
-start()
+const workshopOption = {
+  resource: workshop,
+}
+
+AdminJS.registerAdapter(AdminJSMongoose);
+
+
+// init adminJS
+const adminJS = new AdminJS({
+    databases: [],
+    rootPath: '/admin',
+    resources: [workshopOption, catalogOption],
+});
+const adminJSRouter = AdminJSExpress.buildRouter(adminJS);
+
+// mount adminJS route and run express app
+const app = express();
+app.use(adminJS.options.rootPath, adminJSRouter);
+
+connectDb.connect();
+
+app.listen(8080, () => console.log('AdminJS is under localhost:8080/admin'));
