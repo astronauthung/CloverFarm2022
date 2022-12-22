83de3ea2178dc3a5d78f0dbaffbbc097a2b0a2f5
moonstruck19 <hungnp.21it@vku.udn.vn>
2022-12-22 16:25:06 +0700

    delete something in catalog

--- src/app/models/catalog.js
+++ src/app/models/catalog.js
@@ -2,16 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Catalog = new Schema({
-    catalog_position: { type: Number, maxLength: 255 },
-    catalog_img: { type: String, maxLength: 255 },
-    catalog_data: { type: String, maxLength: 255 },
-
-    catalog_blank: { type: String, maxLength: 255 },
-    catalog_contents: { type: String, maxLength: 255},
-    catalog_contents_title: { type: String, maxLength: 255 },
-    catalog_contents_from: { type: String, maxLength: 255 },
-    catalog_contents_detail: { type: String, maxLength: 255 },
-
     catalog_id: { type: String, maxLength: 255 },
     catalog_name: { type: String, maxLength: 255 },
     catalog_benefit: { type: String, maxLength: 600 },
