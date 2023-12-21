3020bb0dbe40ee19e8736720cd2d1cf238c87277
NickDuyH2K3 <96042373+NickDuyH2K3@users.noreply.github.com>
2023-12-21 10:05:31 +0700

    Config Chatbot ( Adding webhook , changes about interaction )

--- src/app/controllers/chatbotController.js
+++ src/app/controllers/chatbotController.js
@@ -3,32 +3,7 @@ const path = require('path');
 class chatbotController {
     index(req, res, next) {
         res.render('chatbot', {
-            chatbotScript: `
-            <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
-            <script>
-                window.botpressWebChat.init({
-                    "botId": "509d3d24-a977-4252-ae40-5c8d08c4ff19",
-                    "clientId": "509d3d24-a977-4252-ae40-5c8d08c4ff19",
-                    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
-                    "messagingUrl": "https://messaging.botpress.cloud",
-                    "themeName": "prism",
-                    "botName": "Clover",
-                    "containerWidth": "100%25",
-                    "layoutWidth": "100%25",
-
-                    "theme": "prism",
-                    "themeColor": "#2563eb"
-                });
-             
-                window.botpressWebChat.onEvent(
-                    function () {
-                        window.botpressWebChat.sendEvent({ type: "show" });
-                    },
-                    ["LIFECYCLE.LOADED"]
-                );
-            </script>`,
             layout: "chatBotClover",
-            style: "chatbot",
             title: "Chatbot | CLOVER ®",
         });
     }
